7cc23b2165fbd5ea565daca827f4783f2dbb5112
Mark Vasilkov <mvasilkov@gmail.com>
2013-08-20 19:56:58 +0300

    Basics

--- permalink.js
+++ permalink.js
@@ -1,5 +1,26 @@
 (function () {
     function fmt(input, options) {
+        if (typeof input != 'string')
+            return ''
+
+        var separator = typeof options == 'string'? options: '-',
+            beforeNext = false,
+            result = ''
+
+        for (var r = 0; r < input.length; ++r) {
+            var c = input[r]
+
+            if (/[a-zA-Z0-9]/.test(c)) {
+                if (beforeNext) {
+                    result += separator
+                    beforeNext = false
+                }
+                result += c
+            }
+            else beforeNext = true
+        }
+
+        return result.toLowerCase()
     }
 
     if (typeof module != 'undefined' && module.exports)
--- test/basic.js
+++ test/basic.js
@@ -5,4 +5,20 @@ describe('permalink (basic)', function () {
     it('should be a function', function () {
         fmt.should.be.a('function')
     })
+
+    it('should return an empty string for bad inputs', function () {
+        fmt().should.equal('')
+        fmt({bad: 'wrong'}).should.equal('')
+        fmt(null).should.equal('')
+    })
+
+    it('should return readable urls', function () {
+        fmt('hello, world').should.equal('hello-world')
+        fmt('hello, world', '_').should.equal('hello_world')
+        fmt('Visual Basic .NET').should.equal('visual-basic-net')
+        fmt('Visual Basic .NET', '_').should.equal('visual_basic_net')
+    })
+
+    it('', function () {
+    })
 })
